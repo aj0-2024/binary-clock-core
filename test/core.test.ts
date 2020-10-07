7b4533bf6e342da92cf27422006eda49ee34fc15
Anirudh Janga <j.anirudh@icloud.com>
2020-10-06 22:21:30 -0500

    Logging the clock

--- test/core.test.ts
+++ test/core.test.ts
@@ -3,5 +3,6 @@ import { createBinaryClock } from '../src';
 describe('Core Binary Clock Tests', () => {
     it('Creates successfully', () => {
         const clock = createBinaryClock(new Date());
+        console.log(clock.getHours);
     });
 });
